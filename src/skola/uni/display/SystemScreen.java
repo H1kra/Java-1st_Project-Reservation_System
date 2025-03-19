ca9d915fbacf43f2a3bf51a2851c523854572e23
Milan Souček <milan.soucek.2004@gmail.com>
2025-03-19 18:06:32 +0100

    modules

---
+++ src/skola/uni/display/SystemScreen.java
@@ -0,0 +1,19 @@
+package skola.uni.display;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class ReservationSystemScreen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("|-------- Reservation  System --------|");
+        System.out.println("|                                     |");
+        System.out.println("| 1. Create Reservation    6. ------- |");
+        System.out.println("| 2. Delete Reservation    7. ------- |");
+        System.out.println("| 3. Update Reservation    8. ------- |");
+        System.out.println("| 4. Print Reservation     9. ------- |");
+        System.out.println("| 5. Print Reservations    10. Exit   |");
+        System.out.println("|                                     |");
+    }
+}
