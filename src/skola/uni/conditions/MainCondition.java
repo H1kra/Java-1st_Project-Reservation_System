3c81a8895109c62c52493c7bc2523bdbc3758329
Milan Souček <milan.soucek.2004@gmail.com>
2025-03-21 13:15:16 +0100

    prototyping

--- src/skola/uni/conditions/MainCondition.java
+++ src/skola/uni/conditions/MainCondition.java
@@ -1,4 +1,37 @@
 package skola.uni.conditions;
+import skola.uni.input.ReservationInput;
 
-public class mainActionCondition {
+
+public class MainCondition {
+    ReservationInput resIn = new ReservationInput();
+    public void actionCondition(String action) {
+        switch (action) {
+            case "1":
+                resIn.createInput();
+                break;
+            case "2":
+                System.out.println("Work in progress. Try again later.");
+                break;
+            case "3":
+                resIn.deleteInput();
+                break;
+            case "4":
+                resIn.printInput();
+                break;
+            case "5":
+                resIn.printAllInput();
+                break;
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                System.out.println("Work in progress. Try again later.");
+                break;
+            case "10":
+                System.out.println("Exiting...");
+                return;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
 }
